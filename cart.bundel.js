3f4f52d761e1f5cb2e3df157e1a5652fe322ab12
AymanMorsy <aymanmorsy.eg@gmail.com>
2022-06-17 22:55:27 +0200

    v1 compressed

--- cart.bundel.js
+++ cart.bundel.js
@@ -1,9 +1,2 @@
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*********************!*\
-  !*** ./src/cart.js ***!
-  \*********************/
 console.log("cart");
-/******/ })()
-;
 //# sourceMappingURL=cart.bundel.js.map
